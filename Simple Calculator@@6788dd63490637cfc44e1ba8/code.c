b45a838a731ced2470981930cc3328bd0d79bcce
Tamanna Rana <tamanna.rana0917@gmail.com>
2025-02-12 20:14:04 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -11,17 +11,17 @@ int main() {
 
     switch (operator) {
         case '+':
-            printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            printf("%d + %d = %d\n", num1, num2, num1 + num2,"+");
             break;
         case '-':
-            printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            printf("%d - %d = %d\n", num1, num2, num1 - num2,"-");
             break;
         case '*':
-            printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            printf("%d * %d = %d\n", num1, num2, num1 * num2,"*");
             break;
         case '/':
             if (num2 != 0) {
-                printf("%d / %d = %d\n", num1, num2, num1 / num2);
+                printf("%d / %d = %d\n", num1, num2, num1 / num2,"/");
             } else {
                 printf("error\n");
             }

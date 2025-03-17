3370f4c31fbf28a142b7cc8c75d1721659989902
Tamanna Rana <tamanna.rana0917@gmail.com>
2025-03-17 17:41:20 +0530

    Solution to Concatenate Two Strings

--- Concatenate Two Strings@@676c039d80e1714600f67644/code.c
+++ Concatenate Two Strings@@676c039d80e1714600f67644/code.c
@@ -5,12 +5,12 @@ void concatenateStrings(char str1[], char str2[]) {
     int i = 0, j = 0;
 
     // Find the end of the first string
-    while (str1[i] != '\0') {
+    while (str1[i] != '0') {
         i++;
     }
 
     // Append the second string to the first
-    while (str2[j] != '\0') {
+    while (str2[j] != '0') {
         str1[i] = str2[j];
         i++;
         j++;
